0f3a5fe29b0cf48bdd0add92745b44b2c1e41ccd
ksit <phphtmlhub@gmail.com>
2018-04-04 20:51:02 +0530

    first update

---
+++ scripts.ts
@@ -0,0 +1,10 @@
+class Startup {
+    public static main(): number {
+        console.log('Hello World of Typescript');
+        console.log('Typescript is a typed supperset of Javascript that compiles to plain javascript and runs on any brower, any host, any os. this open source.');
+        console.log('Dharmendra kumar');
+        return 0;
+    }
+}
+
+Startup.main();
