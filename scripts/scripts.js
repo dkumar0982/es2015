f21f0e5160073aac568c545bc0338e6c3cd04cf2
ksit <phphtmlhub@gmail.com>
2018-04-04 20:31:05 +0530

    first upload

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log('Hello World of Typescript');
+        console.log('Typescript is a typed supperset of Javascript that compiles to plain javascript and runs on any brower, any host, any os. this open source.');
+        console.log('Dharmendra kumar');
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
+//# sourceMappingURL=scripts.js.map
